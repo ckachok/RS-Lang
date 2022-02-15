8f7621ed9e7b2beaed5417fa82ad871f703fcec8
ckachok <mefelion89@gmail.com>
2022-02-15 14:42:05 +0300

    feat: add component - footer

---
+++ rs-lang/src/pages/_constants.ts
@@ -0,0 +1,28 @@
+const PAGE_INFO = {
+  home: {
+    hash: '#home',
+    id: 'home-page'
+  },
+  textbook: {
+    hash: '#textbook',
+    id: 'textbook-page'
+  },
+  audioCall: {
+    hash: '#audio-call',
+    id: 'audio-call-page'
+  },
+  sprint: {
+    hash: '#sprint',
+    id: 'sprint-page'
+  },
+  statistics: {
+    hash: '#statistics',
+    id: 'statistics-page'
+  },
+  aboutTeam: {
+    hash: '#about-team',
+    id: 'about-team-page'
+  },
+};
+
+export default PAGE_INFO;
