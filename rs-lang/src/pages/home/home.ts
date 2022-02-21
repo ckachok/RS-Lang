6e4f0ef4ce8b9967d7c8f1cc11d6932076dabc19
ckachok <mefelion89@gmail.com>
2022-02-21 08:34:26 +0300

    feat: add page - home

--- rs-lang/src/pages/home/home.ts
+++ rs-lang/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import BaseComponent from 'common-components/base-component';
 import Page from 'pages/page';
+import AppFeature from 'pages/home/components/app-feature/app-feature';
+import FEATURES from 'pages/home/components/app-feature/_constants';
 import 'pages/home/home.scss';
 
 class HomePage extends Page {
@@ -7,9 +9,22 @@ class HomePage extends Page {
     super(parentNode, id);
   }
 
+  private createAppFeatures(parentNode: HTMLElement): void {
+    FEATURES.forEach(item => {
+      const feature = new AppFeature(parentNode, 'div', 'feature', item);
+    });
+  }
+
+  private createDecorativeCircles(parentNode: HTMLElement): void {
+    const decorativeSmallCircle = new BaseComponent(parentNode, 'div', 'decorative-circle decorative-circle_small');
+    const decorativeBigCircle = new BaseComponent(parentNode, 'div', 'decorative-circle decorative-circle_big');
+  }
+
   protected createMain(): void {
-    const main = new BaseComponent(this.parentNode, 'main', 'main');
-    const mainContainer = new BaseComponent(main.node, 'div', 'container main__container home-container').node;
+    this.main = new BaseComponent(this.parentNode, 'main', 'main');
+    const mainContainer = new BaseComponent(this.main.node, 'div', 'container main__container home-container').node;
+    this.createAppFeatures(mainContainer);
+    this.createDecorativeCircles(mainContainer);
   }
 }
 
