f7b92f12fb60731ac264b7bef105c9107b280021
ckachok <mefelion89@gmail.com>
2022-02-21 08:45:50 +0300

    feat: add page - about-team

--- rs-lang/src/pages/about-team/about-team.ts
+++ rs-lang/src/pages/about-team/about-team.ts
@@ -1,5 +1,8 @@
 import BaseComponent from 'common-components/base-component';
 import Page from 'pages/page';
+import Developer from 'pages/about-team/components/developer/developer';
+import PAGE_INFO from 'pages/_constants';
+import DEVELOPERS_DATA from 'pages/about-team/components/developer/_constants';
 import 'pages/about-team/about-team.scss';
 
 class AboutTeamPage extends Page {
@@ -7,9 +10,17 @@ class AboutTeamPage extends Page {
     super(parentNode, id);
   }
 
-  protected createMain(): void {
-    const main = new BaseComponent(this.parentNode, 'main', 'main');
-    const mainContainer = new BaseComponent(main.node, 'div', 'container main__container home-container').node;
+  private createDevelopers(parentNode: HTMLElement) {
+    DEVELOPERS_DATA.forEach(devData => {
+      const developer = new Developer(parentNode, 'div', 'developer', devData);
+    });
+  }
+
+  protected createMain() {
+    this.main = new BaseComponent(this.parentNode, 'main', 'main');
+    const mainContainer = new BaseComponent(this.main.node, 'div', 'container main__container about-team__container').node;
+    const pageTitle = new BaseComponent(mainContainer, 'h2', 'about-team__title', PAGE_INFO.aboutTeam.name);
+    this.createDevelopers(mainContainer);
   }
 }
 
