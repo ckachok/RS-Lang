1c9808a1f70315f7cc20ab4130e32b725e57f616
ckachok <mefelion89@gmail.com>
2022-02-22 05:59:46 +0300

    refactor - update page class

--- rs-lang/src/pages/page.ts
+++ rs-lang/src/pages/page.ts
@@ -3,14 +3,15 @@ import Header from 'common-components/header/header';
 import Footer from 'common-components/footer/footer';
 import Cover from 'common-components/cover/cover';
 import { makeActive, makeInactive } from 'utils/secondary-functions';
-// import Authorization from 'common-components/authorization/authorization';
+import Authorization from 'common-components/authorization/authorization';
+
 abstract class Page {
   protected parentNode: HTMLElement;
   protected header: Header;
   protected main: BaseComponent<HTMLElement>;
   protected footer: Footer;
   private cover: Cover;
-  // private authorization: Authorization;
+  private authorization: Authorization;
 
   constructor(parentNode: HTMLElement, id: string) {
     this.parentNode = parentNode;
@@ -19,14 +20,14 @@ abstract class Page {
     this.createMain();
     this.footer = new Footer(this.parentNode, 'footer', 'footer');
     this.cover = new Cover(this.parentNode, 'div', 'cover');
-    // this.authorization = new Authorization(this.parentNode, 'div', 'authorization');
+    this.authorization = new Authorization(this.parentNode, 'div', 'authorization');
     this.startMenuInteractionCycle();
     this.startUserAuthorizationCycle();
     this.startCoverInteractionCycle();
   }
 
   private startUserAuthorizationCycle(): void {
-    // this.header.onAuthButton = () => makeActive(this.authorization.node, this.cover.node);
+    this.header.onAuthButton = () => makeActive(this.authorization.node, this.cover.node);
   }
 
   private startMenuInteractionCycle(): void {
@@ -34,7 +35,7 @@ abstract class Page {
   }
 
   private startCoverInteractionCycle(): void {
-    // this.cover.onCover = () => makeInactive(this.header.menu, this.header.burgerMenu, this.authorization.node);
+    this.cover.onCover = () => makeInactive(this.header.menu, this.header.burgerMenu, this.authorization.node);
   }
 
   protected startPageRefreshCycle(): void {
